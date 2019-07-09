7609e1ed5d4f02201b9ad484db01dbc927e32c0b
avsingh999 <201651011@iiitvadodara.ac.in>
2019-07-09 11:59:05 +0530

    update

--- src/app/pages/matchmaker-profile/matchmaker-profile.component.ts
+++ src/app/pages/matchmaker-profile/matchmaker-profile.component.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { MatDialogModule } from '@angular/material';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -16,7 +17,7 @@ export class MatchmakerProfileComponent implements OnInit {
   res : any;
   member : any =[];
   getTouch:FormGroup;
-  constructor(private http : HttpClient,private _formBuilder: FormBuilder,config: NgbModalConfig, private modalService: NgbModal) {
+  constructor(private http : HttpClient,private _formBuilder: FormBuilder,config: NgbModalConfig, private modalService: NgbModal, private route: ActivatedRoute) {
      config.backdrop = 'static';
     config.keyboard = false;
         this. getTouch= this._formBuilder.group({
@@ -26,16 +27,17 @@ export class MatchmakerProfileComponent implements OnInit {
   }
 
   ngOnInit() {
-   
+   const matchmaker_id: string = this.route.snapshot.queryParamMap.get('matchmaker_id');
+   console.log(matchmaker_id)
     const headers = new HttpHeaders({
       'Content-Type': 'Application/json',  
       // 'id' : localStorage.getItem('mmID'),
     })
 
   
-   this.http.get('http://matchmakerz.in/api/v1/client/get-matchmaker?id='+localStorage.getItem('mmID'), {headers : headers}).subscribe((res) => {
+   this.http.get('http://matchmakerz.in/api/v1/client/get-matchmaker?id='+matchmaker_id, {headers : headers}).subscribe((res) => {
      this.member=res;
-     console.log(res);
+     console.log(this.member.data[0]);
    })
   }
   open(content) {
